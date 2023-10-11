3e4660c39ad291854f85a348261dc59f3707af32
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-10-11 19:36:36 +0000

    stills

--- src/app/components/Chip.client.js
+++ src/app/components/Chip.client.js
@@ -2,7 +2,7 @@
 // CounterButton.js
 import styled from "styled-components";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Container = styled.div`
   background-color: tomato;
@@ -14,14 +14,30 @@ const Container = styled.div`
   background-position: 0px -316px;
 `;
 
+const stills = [
+  { x: 0, y: -316 },
+  { x: -60, y: -316 },
+  { x: -120, y: 316 },
+];
+
 function Chip() {
-  //const [count, setCount] = useState(0);
+  const [color, setColor] = useState("green");
+  const [still, setStill] = useState(0);
 
-  //const handleButtonClick = () => {
-  //setCount((prevCount) => prevCount + 1);
-  //};
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setStill((prevStill) => prevStill + 1);
+      setColor((prevColor) => (prevColor === "green" ? "red" : "green")); // переключение между красным и зеленым
+    }, 1000);
 
-  return <Container style={{ backgroundColor: "green" }}></Container>;
+    return () => clearInterval(intervalId); // Очищаем интервал, когда компонент размонтируется
+  }, []); // Пустой массив зависимостей означает, что этот useEffect будет запущен один раз при монтировании компонент
+
+  useEffect(() => {
+    console.log(still); // Отслеживаем изменение переменной "still"
+  }, [still]); // Этот useEffect будет запускаться каждый раз, когда значение "still" изменяется
+
+  return <Container style={{ backgroundColor: color }}></Container>;
 }
 
 export default Chip;
