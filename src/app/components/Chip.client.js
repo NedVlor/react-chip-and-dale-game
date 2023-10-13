308fe28b13b9de6b9344afafe6b946d44d78eea2
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-10-13 17:48:58 +0000

    stills

--- src/app/components/Chip.client.js
+++ src/app/components/Chip.client.js
@@ -17,7 +17,19 @@ const Container = styled.div`
 const stills = [
   { x: 0, y: -316 },
   { x: -60, y: -316 },
-  { x: -120, y: 316 },
+  { x: -120, y: -316 },
+  { x: -180, y: -316 },
+  { x: -240, y: -316 },
+  { x: -298, y: -316 },
+  { x: -352, y: -316 },
+  { x: -407, y: -316 },
+  { x: -463, y: -316 },
+  { x: -520, y: -316 },
+  { x: -582, y: -316 },
+  { x: -647, y: -316 },
+  { x: -716, y: -316 },
+  { x: -778, y: -316 },
+  { x: -829, y: -316 },
 ];
 
 function Chip() {
@@ -37,7 +49,12 @@ function Chip() {
     console.log(still); // Отслеживаем изменение переменной "still"
   }, [still]); // Этот useEffect будет запускаться каждый раз, когда значение "still" изменяется
 
-  return <Container style={{ backgroundColor: color }}></Container>;
+  const style = {
+    backgroundColor: color,
+    backgroundPosition: `${stills[still].x}px ${stills[still].y}px`,
+  };
+
+  return <Container style={style}></Container>;
 }
 
 export default Chip;
