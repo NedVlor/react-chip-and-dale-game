5016f1fbc366fa19a387b95685bd294ce8cbbe73
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-11-07 18:39:31 +0000

    refactoring

--- src/app/components/Game.client.js
+++ src/app/components/Game.client.js
@@ -2,6 +2,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import Chip from "./Chip.client.js";
 import styled from "styled-components";
+import { getSolidList } from "./data.js";
 
 const GameContainer = styled.div`
   height: 800px;
@@ -30,15 +31,7 @@ function Game() {
   });
 
   const rChar = useRef(null);
-  const solidList = [
-    { left: 0, top: 633, width: 212, height: 67, ref: useRef(null) },
-    { left: 0, top: 700, width: 408, height: 8, ref: useRef(null) },
-    { left: 300, top: 300, width: 100, height: 100, ref: useRef(null) },
-    { left: 479, top: 700, width: 521, height: 19, ref: useRef(null) },
-    { left: 0, top: 337, width: 14, height: 356, ref: useRef(null) },
-    { left: 500, top: 474, width: 6000, height: 17, ref: useRef(null) },
-    { left: 663, top: 638, width: 65, height: 63, ref: useRef(null) },
-  ];
+  const solidList = getSolidList(useRef);
   const checkIntersection = () => {
     let bariers = []; // for gravity
     let intersection = false; // for X
