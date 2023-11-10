020f5a0bf3f56fd1579607c5edde8176b497de22
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-11-10 19:38:13 +0000

    clean up

--- src/app/components/data.js
+++ src/app/components/data.js
@@ -1,11 +1,10 @@
 export const getSolidList = (useRef) => [
   { left: 0, top: 633, width: 212, height: 67, ref: useRef(null) },
   { left: 0, top: 700, width: 408, height: 8, ref: useRef(null) },
-  { left: 300, top: 300, width: 100, height: 100, ref: useRef(null) },
   { left: 479, top: 700, width: 521, height: 19, ref: useRef(null) },
   { left: 1050, top: 700, width: 521, height: 19, ref: useRef(null) },
   { left: 0, top: 337, width: 14, height: 356, ref: useRef(null) },
-  { left: 500, top: 474, width: 100, height: 17, ref: useRef(null) },
+
   { width: 69, height: 69, left: 661, top: 632, ref: useRef(null) },
   { width: 69, height: 69, left: 800, top: 632, ref: useRef(null) },
 ];
